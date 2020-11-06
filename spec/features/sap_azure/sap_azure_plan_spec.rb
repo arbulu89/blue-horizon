ba6ca93b56c8c6de1232b1cbcdf204b68692b733
Stefano Torresi <stefano@torresi.io>
2020-11-06 16:25:39 +0100

    full coverage of the plan page with the sap-azure template

--- spec/features/sap_azure/sap_azure_plan_spec.rb
+++ spec/features/sap_azure/sap_azure_plan_spec.rb
@@ -10,15 +10,61 @@ describe 'sap azure plan', type: :feature do
   end
 
   it 'displays various sections of the plan' do
-    visit(plan_path)
+    visit plan_path
 
-    expect(page).to have_selector('#plan')
+    expect(page).to have_selector '#plan'
+    plan_block = find '#plan'
 
-    plan_block = find('#plan')
-    expect(plan_block).to have_text 'System settings'
-    expect(plan_block).to have_text 'Resource group'
-    expect(plan_block).to have_text 'HANA nodes'
-    expect(plan_block).to have_text 'Virtual networks'
-    expect(plan_block).to have_text 'Security group'
+    # The following assertions are quite specific to the fixture used.
+    # If you find having to change these too often, consider looking up
+    # all the values dynamically from the plan
+
+    expect(plan_block).to have_selector '.system-settings'
+    expect(plan_block).to have_selector '.resource-group'
+    expect(plan_block).to have_selector '.hana-nodes'
+    expect(plan_block).to have_selector '.virtual-networks'
+    expect(plan_block).to have_selector '.monitoring-server'
+    expect(plan_block).to have_selector '.security-group'
+
+    system_settings = plan_block.find '.system-settings'
+    expect(system_settings).to have_text "System identifier: PRD"
+    expect(system_settings).to have_text "Instance number: 00"
+    expect(system_settings).to have_text "Admin user: prdadm"
+    expect(system_settings).to have_text "Admin password: .Password1"
+
+    resource_group = plan_block.find '.resource-group'
+    expect(resource_group).to have_text "Name: rg-ha-sap-test"
+    expect(resource_group).to have_text "Region: westeurope"
+
+    hana_nodes = plan_block.find '.hana-nodes'
+    expect(hana_nodes).to have_selector '.hana-node', count: 2
+    hana_nodes.find_all('.hana-node').each_with_index do |hana_node,i|
+      expect(hana_node).to have_text "Name: vmhana0#{i+1}"
+      expect(hana_node).to have_text "Private IP address: 10.74.1.1#{i}"
+      expect(hana_node).to have_text 'OS Image: sles-sap-15-sp2'
+      expect(hana_node).to have_selector '.disk', count: 7
+      hana_node.find_all('.disk').each_with_index do |hana_disk,j|
+        expect(hana_disk).to have_text "disk-hana0#{i+1}-Data0#{j+1} (128GB)"
+      end
+    end
+
+    virtual_networks = plan_block.find '.virtual-networks'
+    expect(virtual_networks).to have_selector '.network', count: 2
+    networks = virtual_networks.find_all '.network'
+    expect(networks[0]).to have_text 'Name: vnet-test'
+    expect(networks[0]).to have_text 'Address range: 10.74.0.0/16'
+    expect(networks[1]).to have_text 'Name: snet-test'
+    expect(networks[1]).to have_text 'Address range: 10.74.1.0/24'
+
+    monitoring_server = plan_block.find '.monitoring-server'
+    expect(monitoring_server).to have_text 'Name: vmmonitoring'
+    expect(monitoring_server).to have_text 'Private IP address: 10.74.1.5'
+    expect(monitoring_server).to have_text 'OS Image: sles-sap-15-sp2'
+    expect(monitoring_server).to have_selector '.disk', count: 1
+    expect(monitoring_server.find('.disk')).to have_text "disk-monitoring-Data01 (10GB)"
+
+    security_group = plan_block.find '.security-group'
+    expect(security_group).to have_text 'Name: nsg-test'
+    expect(security_group).to have_selector '.rules .rule'
   end
 end
