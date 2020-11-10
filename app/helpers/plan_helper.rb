1c8698c2367c2e87920cd33a7c62f9a983f86974
Stefano Torresi <stefano@torresi.io>
2020-11-10 17:14:44 +0100

    cs fix

--- app/helpers/plan_helper.rb
+++ app/helpers/plan_helper.rb
@@ -45,8 +45,8 @@ module PlanHelper
   def find_resources_recursively(tf_module)
     # ignore data and null resources and convert the resources array to a hash, allowing lookup by address
     resources = (tf_module['resources'] || [])
-      .select { |tf_resource| tf_resource['type'] != 'null_resource' && tf_resource['mode'] == 'managed' }
-      .map { |tf_resource| [tf_resource['address'], tf_resource] }.to_h
+                .select { |tf_resource| tf_resource['type'] != 'null_resource' && tf_resource['mode'] == 'managed' }
+                .index_by { |tf_resource| tf_resource['address'] }
 
     (tf_module['child_modules'] || [])
       .each { |tf_child_module| resources = resources.merge(find_resources_recursively(tf_child_module)) }
