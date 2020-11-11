a1adf3686c8ccba62d3f08d1448459270c64c874
Stefano Torresi <stefano@torresi.io>
2020-11-11 18:45:06 +0100

    cover the resource_icon view helper

--- spec/helpers/sap_azure_plan_helper_spec.rb
+++ spec/helpers/sap_azure_plan_helper_spec.rb
@@ -12,4 +12,29 @@ describe SapAzurePlanHelper do
       end
     end
   end
+
+  describe 'resource_icon' do
+    resource_type_icon_map = {
+      'azurerm_security_group'                                     => 'security',
+      'azurerm_subnet_network_security_group_association'          => 'security',
+      'azurerm_network_security_rule'                              => 'network_policy',
+      'azurerm_subnet'                                             => 'network',
+      'azurerm_network_interface'                                  => 'ip',
+      'azurerm_public_ip'                                          => 'ip',
+      'azurerm_virtual_machine'                                    => 'node',
+      'tls_private_key'                                            => 'vpn_key',
+      'azurerm_storage_account'                                    => 'storage',
+      'azurerm_resource_group'                                     => 'namespace',
+      'azurerm_subnet_route_table_association'                     => 'abstract',
+      'azurerm_network_interface_backend_address_pool_association' => 'abstract',
+      'foobar'                                                     => 'abstract'
+    }
+
+    resource_type_icon_map.each do |type, icon|
+      it "returns the '#{icon}' icon for the '#{type}' type" do
+        resource = { 'type' => type }
+        expect(helper.resource_icon(resource)).to eq icon
+      end
+    end
+  end
 end
