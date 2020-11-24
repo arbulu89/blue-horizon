b560dff92a493cef97dcad6b0ac46063c8abb042
Stefano Torresi <stefano@torresi.io>
2020-11-24 19:44:16 +0100

    remove attr_writer since it's not actually needed

--- app/validators/source_validator.rb
+++ app/validators/source_validator.rb
@@ -2,12 +2,12 @@
 
 # runs terraform validate with a memoized Terraform instance
 class SourceValidator < ActiveModel::Validator
-  attr_writer :terraform
-
   def validate(record)
     error_msg = terraform.validate(true)
     record.errors[:terraform_syntax] << error_msg if error_msg
   end
+
+  private
 
   def terraform
     @terraform ||= Terraform.new
