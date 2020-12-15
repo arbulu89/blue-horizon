579cd2e4b976c0b34f8989b83bdf88de29b09b6a
Xabier Arbulu Insausti <xarbulu@suse.com>
2020-12-15 13:59:15 +0100

    Update icon tooltip to copied on click

--- app/assets/javascripts/resources.js
+++ app/assets/javascripts/resources.js
@@ -1,6 +1,15 @@
 $(function() {
-  $('.ssh-command').click(function(e) {
-    text = $(this).find('input')[0].value;
+  $('.ssh-command').find('.input-group-button').click(function() {
+    text = $(this).parent().find('input')[0].value;
+
+    // Change tooltip text to copied
+    updated_text = $(this).find('i')
+      .attr('data-copied-hint');
+    $(this).find('i')
+      .tooltip('hide')
+      .attr('data-original-title', updated_text)
+      .tooltip('show');
+
     // Create a dummy input to copy the string array inside it
     var clipboard = document.createElement('input');
     document.body.appendChild(clipboard);
@@ -9,5 +18,13 @@ $(function() {
     clipboard.select();
     document.execCommand('copy');
     document.body.removeChild(clipboard);
-    });
+  });
+
+  $('.ssh-command').find('.input-group-button').mouseout(function() {
+    initial_text = $(this).find('i')
+      .attr('data-initial-hint');
+    $(this).find('i')
+      .attr('data-original-title', initial_text)
+      .tooltip('hide');
+  });
 });
