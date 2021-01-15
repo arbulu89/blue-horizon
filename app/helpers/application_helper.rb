2123fe64e370b95bd758c9c6b930bf978c8f5a9b
Xabier Arbulu Insausti <xarbulu@suse.com>
2021-01-15 13:24:10 +0100

    Implemente non supported badge usage for dashboards

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,6 +17,15 @@ module ApplicationHelper
     end.join.html_safe
   end
 
+  def bootstrap_unsupported(request_id=nil)
+    Rails.configuration.x.top_menu_items.collect do |menu_item|
+      if menu_item['key'].to_s == request_id.to_s && menu_item['unsupported'].present?
+        return render('layouts/unsupported').html_safe
+      end
+    end
+    return
+  end
+
   def custom_image_exists?(filename)
     base_path = Rails.root.join('vendor', 'assets', 'images')
     File.exist?(File.join(base_path, "#{filename}.svg"))   ||
